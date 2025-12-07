3b549511711dbc4e2dd44d433b507dbc9005b58d
Arif <dopymonster@gamil.com>
2025-12-07 16:20:54 +0800

    second

---
+++ src/components/Main.components.jsx
@@ -0,0 +1,53 @@
+import {
+  Form,
+  Button,
+  InputGroup,
+  Container,
+  ListGroup,
+} from "react-bootstrap";
+import "../index.css";
+import { useState } from "react";
+
+
+const Main = () => {
+
+	const [name,setNama] = useState("")
+	const [hasilNama,setHasilNama] = useState("")
+
+	const [namaOrang,setnamaOrang] = useState("")
+	const [listOrang,setListOrang] = useState([])
+
+  return (
+    <Container>
+      <InputGroup className="shadow mt-3">
+        <Form.Control
+          onChange={(e) => setNama(e.target.value)}
+          type="text"
+          placeholder="Name..."
+        />
+        <Button variant="danger" onClick={() => setHasilNama(name)}>
+          SAVE
+        </Button>
+      </InputGroup>
+
+      <h1 className="text-center mt-2">{hasilNama}</h1>
+
+      <InputGroup className="shadow mt-3">
+        <Form.Control value={null} onChange={(e) => setnamaOrang(e.target.value)} type="text" placeholder="Name..." />
+        <Button onClick={() => setListOrang([namaOrang,...listOrang])} variant="success">SAVE</Button>
+      </InputGroup>
+
+      <ListGroup className="mt-2">
+			{listOrang.map((value,i) => {
+				return(
+					<ListGroup.Item key={i}>{value}</ListGroup.Item>
+				)
+			})}
+      </ListGroup>
+
+    </Container>
+  );
+
+}
+
+export default Main
